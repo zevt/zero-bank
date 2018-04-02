0ebb7a60d43919dc123ae940de0f27b23b7fad17
Viet Quoc Tran <viet.tran@siemens.com>
2018-04-02 11:55:55 -0500

    init backend

---
+++ Backend/zero-bank/src/main/java/com/zeroexception/zerobank/ZeroBankApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.zerobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroBankApplication.class, args);
+    }
+}
